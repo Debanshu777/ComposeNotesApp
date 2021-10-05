d20e0087b8207284c3f0fd84df93bd349fde82c5
Debanshu Datta <debanshudatta123@gmail.com>
2021-10-06 03:59:12 +0530

    edit Note Screen UI

---
+++ app/src/main/java/com/debanshu777/composeNotesApp/feature_note/presentation/details_note/DetailNoteEvent.kt
@@ -0,0 +1,12 @@
+package com.debanshu777.composeNotesApp.feature_note.presentation.details_note
+
+import androidx.compose.ui.focus.FocusState
+
+sealed class DetailNoteEvent{
+    data class EnteredTitle(val value:String): DetailNoteEvent()
+    data class ChangeTitleFocus(val focusState: FocusState): DetailNoteEvent()
+    data class EnteredContent(val value:String): DetailNoteEvent()
+    data class ChangeContentFocus(val focusState: FocusState): DetailNoteEvent()
+    data class ChangeColor(val color: Int): DetailNoteEvent()
+    object SaveNote: DetailNoteEvent()
+}
